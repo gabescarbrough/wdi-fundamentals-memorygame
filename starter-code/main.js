c4332002d7f7a1621d5ba0c94bf5d2cee8f6f4d7
Gabriel Scarbrough <gabescarbrough@Gabriel-Scarbroughs-MacBook-Pro.local>
2016-07-07 10:30:20 -0400

    Wrote createBoard function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,21 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
+
 if (cardOne === cardTwo) {
 	alert("You found a match!")
 } else {
 	alert("Sorry, try again.")
+}
+
+*/
+
+var board = document.getElementById('game-board');
+
+var createBoard =function(){
+	for (var i = 0; i < 4; i++) {
+		var card = document.createElement('div');
+		document.querySelector('.board').appendChild(card).className="card";
+	};
 }
